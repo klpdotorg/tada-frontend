7fe651f23d82eaeafd1a68659240c636c27621a6
Akshay Kanthi <aksanoble@gmail.com>
2015-11-08 23:55:24 +0530

    fix paths

--- app/main.js
+++ app/main.js
@@ -1,9 +1,9 @@
 /* Main entry point for the app. Start here to understand the UI composition */
 
-require('../../sass/style.scss');
+require('../sass/style.scss');
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Application';
+import App from './components/Application';
 
 console.log('inside entry.js');
 ReactDOM.render(<App/>, document.getElementById('application'));
