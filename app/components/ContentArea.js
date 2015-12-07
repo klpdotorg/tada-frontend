4ffd3be6c1907c2e4a4e0d5dcb146f0565f3a481
subhaklp <subha@klp.org.in>
2015-12-07 22:07:40 +0530

    Added index route and dashboard page

--- app/components/ContentArea.js
+++ app/components/ContentArea.js
@@ -5,9 +5,16 @@ export default class MainContentArea extends React.Component{
   {
     super();
     console.log('getInitialState in ContentArea.js');
-    this.state = {children: 'Welcome Screen'};
+    this.state = {children: 'Loading..'};
   }
 
+  /* Called just before a component is mounted for the first time. Using this to render the default Welcome screen (IndexRoute)*/
+  componentWillMount(){
+    console.log('componentWillMount in MainContentArea', this.props.children);
+    this.setState({children: this.props.children});
+  }
+
+  /* Called when a component is reacting to a props change. Invoked before render is called. */
   componentWillReceiveProps(nextProps){
     console.log('componentWillReceiveProps', nextProps.children);
     this.setState({children: nextProps.children});
--- app/main.js
+++ app/main.js
@@ -1,12 +1,13 @@
 /* Main entry point for the app. Start here to understand the UI composition */
 
-require('../sass/style.scss')
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import App from './components/Application'
-import { Provider } from 'react-redux'
-import tada from './reducers/Tada'
+require('../sass/style.scss');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import App from './components/Application';
+import Dashboard from './components/Dashboard';
+import { Provider } from 'react-redux';
+import tada from './reducers/Tada';
 import { DefaultRoute, Router, Link, Route, RouteHandler, IndexRoute } from 'react-router';
 import LoginHandler from './components/Login';
 import createHistory from 'history/lib/createHashHistory';
@@ -21,7 +22,8 @@ const routes = (
     <Router history={history}>
         
         <Route path="/" component={App}>
-          <IndexRoute component={App} />
+          <IndexRoute component={Dashboard} />
+            <Route path="dashboard" component={Dashboard}/>
             <Route path="district/:districtId" component={LoginHandler}/>
         </Route>
     </Router>
