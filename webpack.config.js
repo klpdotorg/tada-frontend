7cc7e38c006e9cb1f36ef13c1811582f9bbe52b1
Akshay Kanthi <aksanoble@gmail.com>
2016-01-04 13:33:29 +0530

    add webpack-dev-server as npm script

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,20 @@
+var path = require('path');
+
 module.exports = {
-	entry:
-	{
-		App: [ "./app/main.js",
-		"webpack-dev-server/client?http://localhost:8080/"
-		]
-	},
-	output: {
-		path: '/',
-		filename: "bundle.js",
-		publicPath: 'http://localhost:8080/'
-	},
-	module: {
-		loaders: [
-			{test: /\.css$/, loader: "style!css"},
-			{test: /\.scss$/, loader: 'style!css!sass'},
-			{test: /\.js$/, loaders: ['jsx','babel'], exclude: /node_modules/ }]
+    entry: [
+      path.resolve(__dirname, 'app/main.js')
+    ],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    module: {
+      loaders: [
+        {test: /\.css$/, loader: "style!css"},
+        {test: /\.scss$/, loader: 'style!css!sass'},
+        {test: /\.js$/, loaders: ['jsx','babel'], exclude: /node_modules/ }]
     },
     resolve: {
       extensions: ['', '.js', '.json', '.css']
-    },
-    devtool: 'source-map'
-  }
+    }
+};
