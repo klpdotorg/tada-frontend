71f014d1ec7c8ea5f9026f4204c03f1d06f4888a
subhaklp <subha@klp.org.in>
2016-01-14 10:58:45 +0530

    Added schools to the nav tree

---
+++ app/components/InstitutionDetailsScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import TadaStore from '../stores/TadaStore';
+
+
+let Institution = React.createClass({ 
+
+  render() {
+  	var block = TadaStore.getBoundaryDetailsById(this.props.params.blockId);
+    var blockPath = "#" + block.path;
+    var district = TadaStore.getBoundaryDetailsById(this.props.params.districtId);
+    var districtPath = "#" + district.path;
+    var cluster = TadaStore.getBoundaryDetailsById(this.props.params.clusterId);
+    var clusterPath = "#" + cluster.path;
+    var institution = TadaStore.getBoundaryDetailsById(this.props.params.institutionId);
+    var institutionPath = "#" + institution.path;
+    return(
+      <div>
+        <ol className="breadcrumb">
+          <li><a href={districtPath}>{district.name}</a></li>
+          <li> <a href={blockPath}> {block.name}</a></li>
+          <li> <a href={clusterPath}> {cluster.name}</a></li>
+          <li className="active"> {institution.name}</li>
+        </ol>
+        <div className="form-heading heading-border"> Institution Details</div>
+        
+        <p> Name: {institution.name} </p>
+      </div>);
+  }
+});
+
+export default Institution;  
