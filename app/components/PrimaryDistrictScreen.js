57b7ba0d46ab52d2495f3992b160ab4128467e00
Subhashini Srinivasan <subha@klp.org.in>
2016-08-26 11:48:04 +0530

    Added styles and admin controls for district/block screens

--- app/components/PrimaryDistrictScreen.js
+++ app/components/PrimaryDistrictScreen.js
@@ -10,23 +10,23 @@ let PrimaryDistrict = React.createClass({
     var displayelement;
     if(sessionStorage.getItem('isAdmin'))
     {
-      displayelement = <div><h4 className="brand-blue heading-border"> View Details</h4>
+      displayelement = <div><h4 className="brand-blue heading-border-left"> Modify Details</h4>
                        <form className="form-horizontal" role="form">
                         <div className="form-group">
                           <label className="control-label col-sm-2" for="name">District Name:</label>
-                          <div className="col-sm-10">          
+                          <div className="col-sm-2">          
                             <input type="text" className="form-control" id="name" value={boundary.name}/>
                           </div>
                         </div>
                         </form>
-                        <div className="col-md-2"><button type="submit" className="btn btn-primary">Change Name</button></div>
+                        <div className="col-md-2"><button type="submit" className="btn btn-primary">Save</button></div>
                      </div>
     }
     else
     {
-      displayelement = <div><h4 className="heading-err heading-border"> Insufficient Permissions</h4>
+      displayelement = <div><h4 className="heading-err heading-border-left brand-red"> <i className="fa fa-lock brand-red" aria-hidden="true"></i>  Insufficient Permissions</h4>
         <p>You need administrator privileges to modify Boundary details.</p>
-        <h4 className="brand-blue heading-border"> View Details</h4>
+        <h4 className="brand-blue heading-border-left"> District Details</h4>
         <p> Name: {boundary.name} </p></div>
     }
     return(
