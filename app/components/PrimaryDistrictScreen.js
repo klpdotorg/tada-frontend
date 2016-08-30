58fa43827746a286ccba2c4ed881a532d94167e8
Akshay Kanthi <aksanoble@gmail.com>
2016-08-30 12:50:22 +0530

    primary-district: refresh input button

--- app/components/PrimaryDistrictScreen.js
+++ app/components/PrimaryDistrictScreen.js
@@ -13,6 +13,7 @@ export default class SecondaryNavBar extends React.Component {
     };
 
   }
+
   onClickSaveDistrict(districtid,name){
     console.log("on save district clicked");
     console.log(this.districtName.value);
@@ -24,49 +25,48 @@ export default class SecondaryNavBar extends React.Component {
     this.setState({value: event.target.value});
   }
 
-  render() {
-  	var districtId = this.props.params.districtId;
-  	var boundary = this.props.boundaryDetails[districtId];
-  	var districtPath = "#" + boundary.path;
-    var displayelement;
-    this.state.value=boundary.name;
-    if(sessionStorage.getItem('isAdmin'))
-    {
-      displayelement = <div><h4 className="brand-blue heading-border-left"> Modify Details</h4>
-                       <form className="form-horizontal" role="form">
-                        <div className="form-group">
-                          <label className="control-label col-sm-2" htmlFor="name">District Name:</label>
-                          <div className="col-sm-2">          
-                            <input type="text" ref={(ref) => this.districtName = ref} className="form-control" id="name" defaultValue={boundary.name} onChange={(event)=>{this.handleChange(event)}}/>
-                          </div>
-                        </div>
-                        </form>
+  render() {    
+    var districtId = this.props.params.districtId;
+    var boundary = this.props.boundaryDetails[districtId];
+    var districtPath = "#" + boundary.path;
+    var Displayelement;
+    this.state.value = boundary.name;
+    if(sessionStorage.getItem('isAdmin')) {
+      Displayelement = (props) => 
+        <div>
+          <h4 className="brand-blue heading-border-left"> Modify Details</h4>
+            <form className="form-horizontal" role="form">
+              <div className="form-group">
+                <label className="control-label col-sm-2" htmlFor="name">District Name:</label>
+                <div className="col-sm-2">          
+                  <input type="text" ref={(ref) => this.districtName = ref} className="form-control" id="name" defaultValue={boundary.name}/>
+                </div>
+              </div>
+              </form>
 
-                        <div className="col-md-2">
-                          <button type="submit" className="btn btn-primary" onClick={() => {this.onClickSaveDistrict(districtId,"teset") }}>Save</button>
-                          <button type="submit" className="btn btn-primary">Delete</button>
-
-                        </div>
-                       
-                       
-
-
-                     </div>
+              <div className="col-md-2">
+                <button type="submit" className="btn btn-primary" onClick={() => {this.onClickSaveDistrict(districtId,"teset") }}>Save</button>
+                <button type="submit" className="btn btn-primary">Delete</button>
+              </div>
+        </div>
     }
-    else
-    {
-      displayelement = <div><h4 className="heading-err heading-border-left brand-red"> <i className="fa fa-lock brand-red" aria-hidden="true"></i>  Insufficient Permissions</h4>
-        <p>You need administrator privileges to modify Boundary details.</p>
-        <h4 className="brand-blue heading-border-left"> District Details</h4>
-        <p> Name: {boundary.name} </p></div>
+    else {
+      Displayelement = (props) => 
+        <div>
+          <h4 className="heading-err heading-border-left brand-red"> <i className="fa fa-lock brand-red" aria-hidden="true"></i>  Insufficient Permissions</h4>
+          <p>You need administrator privileges to modify Boundary details.</p>
+          <h4 className="brand-blue heading-border-left"> District Details</h4>
+          <p> Name: {boundary.name}</p>
+        </div>
     }
+
     return(
-    	<div>
-    		<ol className="breadcrumb">
-    			<li className="active">{boundary.name}</li>
-    		</ol>
-    		{displayelement} 
-    	</div>
+      <div>
+        <ol className="breadcrumb">
+          <li className="active">{boundary.name}</li>
+        </ol>
+        <Displayelement/>
+      </div>
     );
   }
 }
