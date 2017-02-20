13494bcf2d1bec673a22e9b525fe9d9293177753
Subhashini Srinivasan <subha@klp.org.in>
2017-02-20 10:53:57 +0530

    Change to package.json to enable source map

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     entry: [
       path.resolve(__dirname, 'app/main.js')
     ],
+    devtool: "#cheap-eval-source-map",
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
