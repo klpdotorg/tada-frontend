022c642ec23c70eb684c05b3ddaf609d57b39640
vibhutipratapsingh <vibhutipratapsingh@gmail.com>
2017-04-07 17:39:48 +0530

    preschool loose focus on typing letter issue resolved

--- app/components/Preschool.js
+++ app/components/Preschool.js
@@ -110,9 +110,9 @@ export default class Institution extends React.Component {
                 })
                 dispatch(openNode(params.institutionId))
                 dispatch(fetchEntitiesFromServer(params.institutionId))
-              })      
+              })
             })
-      })  
+      })
     })
 
   }
@@ -181,109 +181,108 @@ export default class Institution extends React.Component {
     this.props.dispatch(deleteInstitution(Number(this.props.params.circleId), Number(this.props.params.institutionId)))
   }
 
-  render() {
+Displayelement = (props) =>{
+  var project = this.props.boundaries.boundaryDetails[this.props.params.projectId];
+  var district = this.props.boundaries.boundaryDetails[this.props.params.districtId];
+  var circle = this.props.boundaries.boundaryDetails[this.props.params.circleId];
+  var institution = this.state.institution
 
-    var project = this.props.boundaries.boundaryDetails[this.props.params.projectId];
-    var district = this.props.boundaries.boundaryDetails[this.props.params.districtId];
-    var circle = this.props.boundaries.boundaryDetails[this.props.params.circleId];
-    var institution = this.state.institution
-    var Displayelement;
+   if(sessionStorage.getItem('isAdmin')) {
+     return(
+        <div>
+         <ol className="breadcrumb">
+            <li><Link to={district.path}>{district.name}</Link></li>
+            <li> <Link to={project.path}> {project.name}</Link></li>
+            <li> <Link to={circle.path}> {circle.name}</Link></li>
+            <li className="active"> {institution.name}</li>
+          </ol>
+              <div>
+            <div className='heading-border-left'>
+              <h4 className="brand-blue col-md-10">Modify Details</h4>
+              <Button onClick={this.toggleClassModal} title='Add Class'/>
+            </div>
+            <form className="form-horizontal" role="form">
+                <div className="form-group">
+                  <label className="control-label col-sm-2" htmlFor="name">Name:</label>
+                  <div className="col-sm-10">
+                    <input type="text" onChange={(e) => {this.setValue(e.target.value, 'name')}} className="form-control" id="name" value={institution.name}/>
+                  </div>
+                </div>
 
-     if(sessionStorage.getItem('isAdmin')) {
-       Displayelement = (props) =>
-      <div>
-       <ol className="breadcrumb">
-          <li><Link to={district.path}>{district.name}</Link></li>
-          <li> <Link to={project.path}> {project.name}</Link></li>
-          <li> <Link to={circle.path}> {circle.name}</Link></li>
-          <li className="active"> {institution.name}</li>
-        </ol>
-            <div>
-          <div className='heading-border-left'>
-            <h4 className="brand-blue col-md-10">Modify Details</h4>
-            <Button onClick={this.toggleClassModal} title='Add Class'/>
+                <div className="form-group">
+                  <label className="control-label col-sm-2" htmlFor="address">Address:</label>
+                  <div className="col-sm-10">
+                    <textarea onChange={(e) => {this.setValue(e.target.value, 'address')}} className="form-control" id="address" rows="3" value={institution.address}>
+                    </textarea>
+                  </div>
+                </div>
+
+                <div className="form-group">
+                  <label className="control-label col-sm-2" htmlFor="area">Area:</label>
+                  <div className="col-sm-10">
+                    <input id="area" onChange={(e) => {this.setValue(e.target.value, 'area')}} type="text" className="form-control" value={institution.area}/>
+                  </div>
+                </div>
+
+                <div className="form-group">
+                  <label className="control-label col-sm-2" htmlFor="landmark">Landmark:</label>
+                  <div className="col-sm-10">
+                    <input onChange={(e) => {this.setValue(e.target.value, 'landmark')}} type="text" className="form-control" id="landmark" value={institution.landmark}/>
+                  </div>
+                </div>
+
+                <div className="form-group">
+                  <label className="control-label col-sm-2" htmlFor="pincode">Pincode:</label>
+                  <div className="col-sm-10">
+                    <input onChange={(e) => {this.setValue(e.target.value, 'pincode')}} type="text" className="form-control" id="pincode" value={institution.pincode}/>
+                  </div>
+                </div>
+
+                <div className="form-group">
+                  <label className="control-label col-sm-2" htmlFor="category">Category:</label>
+                  <div className="col-sm-10">
+                    <Select name="form-field-name" value={institution.cat} options={this.state.institutionCategories.list} onChange={(val) => {this.setValue(val.value, 'cat')}} />
+                  </div>
+                </div>
+
+                <div className="form-group">
+                  <label className="control-label col-sm-2" htmlFor="medium">Medium:</label>
+                  <div className="col-sm-10">
+                    <Select multi name="languages" value={institution.languages} options={this.state.languages.list} onChange={(val) => {this.setValue(val.map(v => v.value), 'languages')}}/>
+                  </div>
+                </div>
+
+               <div className="form-group">
+                  <label className="control-label col-sm-2" htmlFor="gender">Gender:</label>
+                  <div className="col-sm-10">
+                    <select onChange={(e) => {this.setValue(e.target.value, 'institution_gender')}} value={institution.institution_gender} className="form-control" id="gender">
+                      <option value='co-ed'>Co-Ed</option>
+                      <option value='boys'>Boys</option>
+                      <option value='girls'>Girls</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div className="form-group">
+                  <label className="control-label col-sm-2" htmlFor="mgmt">Management:</label>
+                  <div className="col-sm-10">
+                    <Select name="form-field-name" value={institution.mgmt} options={this.state.mgmt.list} onChange={(val) => {this.setValue(val.value, 'mgmt')}} />
+                  </div>
+                </div>
+              </form>
+
+            <div className="col-md-2">
+              <button type="submit" className="btn btn-primary" onClick={this.saveInsti}>Save</button>
+              <button type="submit" className="btn btn-primary" onClick={this.showConfirmation}>Delete</button>
+              <ConfirmModal isOpen={this.state.openConfirmModal} onAgree={this.deleteInstitution} onCloseModal={this.closeConfirmModal} entity={institution.name}/>
+            </div>
           </div>
-          <form className="form-horizontal" role="form">
-              <div className="form-group">
-                <label className="control-label col-sm-2" htmlFor="name">Name:</label>
-                <div className="col-sm-10">
-                  <input type="text" onChange={(e) => {this.setValue(e.target.value, 'name')}} className="form-control" id="name" value={institution.name}/>
-                </div>
-              </div>
-
-              <div className="form-group">
-                <label className="control-label col-sm-2" htmlFor="address">Address:</label>
-                <div className="col-sm-10">
-                  <textarea onChange={(e) => {this.setValue(e.target.value, 'address')}} className="form-control" id="address" rows="3" value={institution.address}>
-                  </textarea>
-                </div>
-              </div>
-
-              <div className="form-group">
-                <label className="control-label col-sm-2" htmlFor="area">Area:</label>
-                <div className="col-sm-10">
-                  <input id="area" onChange={(e) => {this.setValue(e.target.value, 'area')}} type="text" className="form-control" value={institution.area}/>
-                </div>
-              </div>
-
-              <div className="form-group">
-                <label className="control-label col-sm-2" htmlFor="landmark">Landmark:</label>
-                <div className="col-sm-10">
-                  <input onChange={(e) => {this.setValue(e.target.value, 'landmark')}} type="text" className="form-control" id="landmark" value={institution.landmark}/>
-                </div>
-              </div>
-
-              <div className="form-group">
-                <label className="control-label col-sm-2" htmlFor="pincode">Pincode:</label>
-                <div className="col-sm-10">
-                  <input onChange={(e) => {this.setValue(e.target.value, 'pincode')}} type="text" className="form-control" id="pincode" value={institution.pincode}/>
-                </div>
-              </div>
-
-              <div className="form-group">
-                <label className="control-label col-sm-2" htmlFor="category">Category:</label>
-                <div className="col-sm-10">
-                  <Select name="form-field-name" value={institution.cat} options={this.state.institutionCategories.list} onChange={(val) => {this.setValue(val.value, 'cat')}} />
-                </div>
-              </div>
-
-              <div className="form-group">
-                <label className="control-label col-sm-2" htmlFor="medium">Medium:</label>
-                <div className="col-sm-10">
-                  <Select multi name="languages" value={institution.languages} options={this.state.languages.list} onChange={(val) => {this.setValue(val.map(v => v.value), 'languages')}}/>
-                </div>
-              </div>
-
-             <div className="form-group">
-                <label className="control-label col-sm-2" htmlFor="gender">Gender:</label>
-                <div className="col-sm-10">
-                  <select onChange={(e) => {this.setValue(e.target.value, 'institution_gender')}} value={institution.institution_gender} className="form-control" id="gender">
-                    <option value='co-ed'>Co-Ed</option>
-                    <option value='boys'>Boys</option>
-                    <option value='girls'>Girls</option>
-                  </select>
-                </div>
-              </div>
-
-              <div className="form-group">
-                <label className="control-label col-sm-2" htmlFor="mgmt">Management:</label>
-                <div className="col-sm-10">
-                  <Select name="form-field-name" value={institution.mgmt} options={this.state.mgmt.list} onChange={(val) => {this.setValue(val.value, 'mgmt')}} />
-                </div>
-              </div>
-            </form>
-
-          <div className="col-md-2">
-            <button type="submit" className="btn btn-primary" onClick={this.saveInsti}>Save</button>
-            <button type="submit" className="btn btn-primary" onClick={this.showConfirmation}>Delete</button>
-            <ConfirmModal isOpen={this.state.openConfirmModal} onAgree={this.deleteInstitution} onCloseModal={this.closeConfirmModal} entity={institution.name}/>
-          </div>
+          <CreateClass placeHolder='Class Name' title='Create New Class' isOpen={this.props.modal.createClass} onCloseModal={this.toggleClassModal} save={ this.saveClass } />
         </div>
-        <CreateClass placeHolder='Class Name' title='Create New Class' isOpen={this.props.modal.createClass} onCloseModal={this.toggleClassModal} save={ this.saveClass } />
-      </div>
-  }
-    else {
-      Displayelement = () =>
+    )
+}
+  else {
+    return(
         <div>
           <ol className="breadcrumb">
             <li><Link to={district.path}>{district.name}</Link></li>
@@ -296,13 +295,20 @@ export default class Institution extends React.Component {
           <h4 className="brand-blue heading-border-left"> Institution Details</h4>
           <p> Name: {institution.name}</p>
         </div>
-    }
+      )
+  }
+
+}
+  render() {
+    var project = this.props.boundaries.boundaryDetails[this.props.params.projectId];
+    var district = this.props.boundaries.boundaryDetails[this.props.params.districtId];
+    var circle = this.props.boundaries.boundaryDetails[this.props.params.circleId];
+    var institution = this.state.institution
+
     return (
       this.state.isLoading ?
-      <div>Loading...</div> :
-      <Displayelement {...this.props}/>
+      <div>Loading...</div> :<div>{this.Displayelement(...this.props)}</div>
     )
 
   }
 };
-
