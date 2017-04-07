760f6941cb9699a940c892a224d5c36601ced22d
vibhutipratapsingh <vibhutipratapsingh@gmail.com>
2017-04-07 14:03:14 +0530

    removed comments from form validation in update district page

--- app/components/PrimaryDistrict.js
+++ app/components/PrimaryDistrict.js
@@ -95,14 +95,12 @@ export default class PrimaryDistrict extends React.Component {
   }
 
   enableSubmitButton=()=> {
-    console.log("enableSubmitButton");
     this.setState({
       canSubmit: true,
     });
   }
 
   disableSubmitButton=()=> {
-    console.log("disableSubmitButton");
     this.setState({
       canSubmit: false,
     });
@@ -158,47 +156,6 @@ DistrictSummary=(props)=>{
     var boundaryType = boundary.boundary_type;
     var DistrictSummary;
     this.state.value = boundary.name;
-    // if(sessionStorage.getItem('isAdmin')) {
-    //   DistrictSummary = (props) =>
-    //     <div>
-    //       <div className="heading-border-left brand-blue">
-    //         <h4 className="brand-blue brand-bg-blue col-md-10">Modify Details</h4>
-    //         {boundaryType == 2 ? <Button title='Add Project' onClick={this.toggleProjectModal} /> : <Button title='Add Block' onClick={this.toggleBlockModal} />}
-    //       </div>
-    //        {/*<Formsy.Form
-    //          onValidSubmit={this.saveDistrict}
-    //         onValid={this.enableSubmitButton}
-    //         onInvalid={this.disableSubmitButton}
-    //         disabled={this.state.disabled}
-    //         ref={(ref) => this.myform = ref}>
-    //         <Input name="DistrictName" id="DistrictName" value={boundary.name} label="Program" type="text"
-    //           placeholder="Please enter the district name" help="This is a required field" required validations="minLength:1"/>
-    //       </Formsy.Form>*/}
-    //         <form className="form-horizontal boundary-form" role="form">
-    //           <div className="form-group">
-    //             <label className="control-label col-sm-2" htmlFor="name">District Name:</label>
-    //             <div className="col-sm-2">
-    //               <input type="text" ref={(ref) => this.districtName = ref} className="form-control" id="name" defaultValue={boundary.name}/>
-    //             </div>
-    //           </div>
-    //         </form>
-    //
-    //           <div className="col-md-8">
-    //             <button type="button" className="btn btn-primary" onClick={this.saveDistrict}>Save</button>
-    //             <button type="submit" className="btn btn-primary padded-btn" onClick={() => {this.showConfirmation() }}>Delete</button>
-    //             <ConfirmModal isOpen={this.state.openConfirmModal} onAgree={this.deleteDistrict} onCloseModal={this.closeConfirmModal} entity={boundary.name}/>
-    //           </div>
-    //     </div>
-    // }
-    // else {
-    //   DistrictSummary = (props) =>
-    //     <div>
-    //       <h4 className="heading-err heading-border-left brand-red"> <i className="fa fa-lock brand-red" aria-hidden="true"></i>  Insufficient Permissions</h4>
-    //       <p>You need administrator privileges to modify Boundary details.</p>
-    //       <h4 className="brand-blue heading-border-left"> District Details</h4>
-    //       <p> Name: {boundary.name}</p>
-    //     </div>
-    // }
 
     return(
       <div>
