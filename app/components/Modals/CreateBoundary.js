893ee5b9b0901885f66d40f948d60db51b3f29fe
Megha Vishwanath <megha@klp.org.in>
2017-05-25 16:17:46 +0530

    statndardized button on

--- app/components/Modals/CreateBoundary.js
+++ app/components/Modals/CreateBoundary.js
@@ -54,7 +54,7 @@ export default class CreateDistrict extends Component {
               <button type="button" className="close" onClick={this.props.onCloseModal} aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
-              <h4 className="modal-title" id="title">{this.props.title}</h4>
+              <h4 className="text-primary modal-title" id="title">{this.props.title}</h4>
             </div>
             <div className="modal-body">
               <Formsy.Form
@@ -72,7 +72,7 @@ export default class CreateDistrict extends Component {
             </div>
 
             <div className="modal-footer">
-              <button type="button" className="btn btn-default" onClick={this.props.onCloseModal}>Cancel</button>
+              <button type="button" className="btn btn-primary" onClick={this.props.onCloseModal}>Cancel</button>
               <button
                 type="button" disabled={!this.state.canSubmit} className="btn btn-primary" onClick={this.submitForm}
               >Create</button>
